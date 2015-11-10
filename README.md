29341fe424ba78af06d69257eb64ad37feb8362b
unknown <geo_beja@PC-12-3465.geo.hu-berlin.de>
2015-11-10 16:59:24 +0100

    added some readme issues

--- README.md
+++ README.md
@@ -30,6 +30,9 @@ The SenseCarbon Time Series Viewer is a small improvised viewer for remote sensi
 
 ### Contribution guidelines ###
 
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ 
+
 
 ### Who do I talk to? ###
  
--- imagechipviewsettings_widget.py
+++ imagechipviewsettings_widget.py
@@ -7,6 +7,16 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+'''
+/***************************************************************************
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ ***************************************************************************/
+'''
 
 import os
 
