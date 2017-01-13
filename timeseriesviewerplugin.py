76ffd1cace47df364ed25ec399a7f5db4264996b
benjamin.jakimow@geo.hu-berlin.de <q8DTkxUg-BB>
2017-01-13 14:47:13 +0100

    fixed missing error that appeared in QGIS if python console was not activated init qt resource with initGUI

--- timeseriesviewerplugin.py
+++ timeseriesviewerplugin.py
@@ -16,12 +16,15 @@ from PyQt4.QtGui import *
 
 DIR_REPO = os.path.normpath(os.path.split(inspect.getfile(inspect.currentframe()))[0])
 DIR_SITE_PACKAGES = None
-class TSVPlugin:
+class TimeSeriesViewerPlugin:
 
     def __init__(self, iface):
         self.iface = iface
         self.tsv = None
-
+        import console.console as CONSOLE
+        if CONSOLE._console is None:
+            CONSOLE._console = CONSOLE.PythonConsole(iface.mainWindow())
+            QTimer.singleShot(0, CONSOLE._console.activate)
 
 
     def initGui(self):
@@ -29,32 +32,32 @@ class TSVPlugin:
         syspaths = [os.path.normpath(p) for p in sys.path]
         if DIR_REPO not in syspaths: sys.path.append(DIR_REPO)
 
+        import timeseriesviewer.ui.resources_py2
+        timeseriesviewer.ui.resources_py2.qInitResources()
         # add platform independent site-packages
         if DIR_SITE_PACKAGES:
             site.addsitedir(DIR_SITE_PACKAGES)
 
-        from timeseriesviewer.main import TimeSeriesViewer
-
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
-
-        icon = TimeSeriesViewer.icon()
-        action = QAction(icon, self.tr(u'SenseCarbon Time Series Viewer'), self.iface.mainWindow())
+        import timeseriesviewer
+        # init main UI
+        from timeseriesviewer import dprint, DIR_UI, jp
+        icon = QIcon(jp(DIR_UI, *['icons', 'icon.png']))
+        action = QAction(icon, 'SenseCarbon Time Series Viewer', self.iface)
         action.triggered.connect(self.run)
         self.toolbarActions.append(action)
 
 
         for action in self.toolbarActions:
+            print('ADD ICON')
             self.iface.addToolBarIcon(action)
 
     def run(self):
         from timeseriesviewer.main import TimeSeriesViewer
         # open QGIS python console. this is required to allow for print() statements in the source code.
-        if isinstance(self.iface, QgisInterface):
-            import console
-            c = console.show_console()
-            c.setVisible(True)
+
 
         self.tsv = TimeSeriesViewer(self.iface)
+
         self.tsv.run()
 
     def unload(self):
@@ -72,4 +75,4 @@ class TSVPlugin:
 
 
     def tr(self, message):
-        return QCoreApplication.translate('EnMAPBoxPlugin', message)
+        return QCoreApplication.translate('TimeSeriesViewerPlugin', message)
