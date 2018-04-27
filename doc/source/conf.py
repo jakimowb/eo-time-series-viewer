907f6ba23a07aa796de38e8e79e39575cdce3116
benjamin.jakimow <benjamin.jakimow@geo.hu-berlin.de>
2018-04-27 17:37:14 +0200

    tbd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,11 +37,12 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if on_rtd:
     print('Fetching files with git_lfs')
-    from git_lfs import fetch
-
     DOC_SOURCES_DIR = os.path.dirname(os.path.abspath(__file__))
     PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(DOC_SOURCES_DIR))
     sys.path.insert(0, DOC_SOURCES_DIR)
+    print('PROJECT_ROOT_DIR', PROJECT_ROOT_DIR)
+
+    from git_lfs import fetch
     fetch(PROJECT_ROOT_DIR)
 
 
