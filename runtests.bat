9d8006220de491a7e881195fd63d715a14ec4fd9
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2020-01-23 19:50:04 +0100

    update runtests
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- runtests.bat
+++ runtests.bat
@@ -5,22 +5,19 @@ set CI=True
 python3 make/setuprepository.py
 
 mkdir test-reports\today
-python -m nose2 -s tests test_dummy & move nose2-junit.xml test-reports/today/test_dummy.xml
-python -m nose2 -s tests test_fileFormatLoading & move nose2-junit.xml test-reports/today/test_fileFormatLoading.xml
-python -m nose2 -s tests test_init & move nose2-junit.xml test-reports/today/test_init.xml
-python -m nose2 -s tests test_inmemorydata & move nose2-junit.xml test-reports/today/test_inmemorydata.xml
-python -m nose2 -s tests test_labeling & move nose2-junit.xml test-reports/today/test_labeling.xml
-python -m nose2 -s tests test_layerproperties & move nose2-junit.xml test-reports/today/test_layerproperties.xml
-python -m nose2 -s tests test_main & move nose2-junit.xml test-reports/today/test_main.xml
-python -m nose2 -s tests test_mapcanvas & move nose2-junit.xml test-reports/today/test_mapcanvas.xml
-python -m nose2 -s tests test_maptools & move nose2-junit.xml test-reports/today/test_maptools.xml
-python -m nose2 -s tests test_mapvisualization & move nose2-junit.xml test-reports/today/test_mapvisualization.xml
-python -m nose2 -s tests test_qgis_environment & move nose2-junit.xml test-reports/today/test_qgis_environment.xml
-python -m nose2 -s tests test_qgis_interaction & move nose2-junit.xml test-reports/today/test_qgis_interaction.xml
-python -m nose2 -s tests test_resources & move nose2-junit.xml test-reports/today/test_resources.xml
-python -m nose2 -s tests test_sensorvisualization & move nose2-junit.xml test-reports/today/test_sensorvisualization.xml
-python -m nose2 -s tests test_settings & move nose2-junit.xml test-reports/today/test_settings.xml
-python -m nose2 -s tests test_stackedbandinput & move nose2-junit.xml test-reports/today/test_stackedbandinput.xml
-python -m nose2 -s tests test_temporalprofiles & move nose2-junit.xml test-reports/today/test_temporalprofiles.xml
-python -m nose2 -s tests test_timeseries & move nose2-junit.xml test-reports/today/test_timeseries.xml
-python -m nose2 -s tests test_utils & move nose2-junit.xml test-reports/today/test_utils.xml
+python -m nose2 -s tests test_fileFormatLoading #& move nose2-junit.xml test-reports/today/test_fileFormatLoading.xml
+python -m nose2 -s tests test_inmemorydata #& move nose2-junit.xml test-reports/today/test_inmemorydata.xml
+python -m nose2 -s tests test_labeling #& move nose2-junit.xml test-reports/today/test_labeling.xml
+python -m nose2 -s tests test_layerproperties #& move nose2-junit.xml test-reports/today/test_layerproperties.xml
+python -m nose2 -s tests test_main #& move nose2-junit.xml test-reports/today/test_main.xml
+python -m nose2 -s tests test_mapcanvas #& move nose2-junit.xml test-reports/today/test_mapcanvas.xml
+python -m nose2 -s tests test_maptools #& move nose2-junit.xml test-reports/today/test_maptools.xml
+python -m nose2 -s tests test_mapvisualization #& move nose2-junit.xml test-reports/today/test_mapvisualization.xml
+python -m nose2 -s tests test_qgis_environment #& move nose2-junit.xml test-reports/today/test_qgis_environment.xml
+python -m nose2 -s tests test_qgis_interaction #& move nose2-junit.xml test-reports/today/test_qgis_interaction.xml
+python -m nose2 -s tests test_resources #& move nose2-junit.xml test-reports/today/test_resources.xml
+python -m nose2 -s tests test_settings #& move nose2-junit.xml test-reports/today/test_settings.xml
+python -m nose2 -s tests test_stackedbandinput #& move nose2-junit.xml test-reports/today/test_stackedbandinput.xml
+python -m nose2 -s tests test_temporalprofiles #& move nose2-junit.xml test-reports/today/test_temporalprofiles.xml
+python -m nose2 -s tests test_timeseries #& move nose2-junit.xml test-reports/today/test_timeseries.xml
+python -m nose2 -s tests test_utils #& move nose2-junit.xml test-reports/today/test_utils.xml
--- runtests.sh
+++ runtests.sh
@@ -9,22 +9,19 @@ export CI
 python3 make/setuprepository.py
 
 mkdir test-reports/today
-python3 -m nose2 -s tests test_dummy #| mv nose2-junit.xml test-reports/today/test_dummy.xml
-python3 -m nose2 -s tests test_fileFormatLoading #| mv nose2-junit.xml test-reports/today/test_fileFormatLoading.xml
-python3 -m nose2 -s tests test_init #| mv nose2-junit.xml test-reports/today/test_init.xml
-python3 -m nose2 -s tests test_inmemorydata #| mv nose2-junit.xml test-reports/today/test_inmemorydata.xml
-python3 -m nose2 -s tests test_labeling #| mv nose2-junit.xml test-reports/today/test_labeling.xml
-python3 -m nose2 -s tests test_layerproperties #| mv nose2-junit.xml test-reports/today/test_layerproperties.xml
-python3 -m nose2 -s tests test_main #| mv nose2-junit.xml test-reports/today/test_main.xml
-python3 -m nose2 -s tests test_mapcanvas #| mv nose2-junit.xml test-reports/today/test_mapcanvas.xml
-python3 -m nose2 -s tests test_maptools #| mv nose2-junit.xml test-reports/today/test_maptools.xml
-python3 -m nose2 -s tests test_mapvisualization #| mv nose2-junit.xml test-reports/today/test_mapvisualization.xml
-python3 -m nose2 -s tests test_qgis_environment #| mv nose2-junit.xml test-reports/today/test_qgis_environment.xml
-python3 -m nose2 -s tests test_qgis_interaction #| mv nose2-junit.xml test-reports/today/test_qgis_interaction.xml
-python3 -m nose2 -s tests test_resources #| mv nose2-junit.xml test-reports/today/test_resources.xml
-python3 -m nose2 -s tests test_sensorvisualization #| mv nose2-junit.xml test-reports/today/test_sensorvisualization.xml
-python3 -m nose2 -s tests test_settings #| mv nose2-junit.xml test-reports/today/test_settings.xml
-python3 -m nose2 -s tests test_stackedbandinput #| mv nose2-junit.xml test-reports/today/test_stackedbandinput.xml
-python3 -m nose2 -s tests test_temporalprofiles #| mv nose2-junit.xml test-reports/today/test_temporalprofiles.xml
-python3 -m nose2 -s tests test_timeseries #| mv nose2-junit.xml test-reports/today/test_timeseries.xml
-python3 -m nose2 -s tests test_utils #| mv nose2-junit.xml test-reports/today/test_utils.xml
+python -m nose2 -s tests test_fileFormatLoading #| mv nose2-junit.xml test-reports/today/test_fileFormatLoading.xml
+python -m nose2 -s tests test_inmemorydata #| mv nose2-junit.xml test-reports/today/test_inmemorydata.xml
+python -m nose2 -s tests test_labeling #| mv nose2-junit.xml test-reports/today/test_labeling.xml
+python -m nose2 -s tests test_layerproperties #| mv nose2-junit.xml test-reports/today/test_layerproperties.xml
+python -m nose2 -s tests test_main #| mv nose2-junit.xml test-reports/today/test_main.xml
+python -m nose2 -s tests test_mapcanvas #| mv nose2-junit.xml test-reports/today/test_mapcanvas.xml
+python -m nose2 -s tests test_maptools #| mv nose2-junit.xml test-reports/today/test_maptools.xml
+python -m nose2 -s tests test_mapvisualization #| mv nose2-junit.xml test-reports/today/test_mapvisualization.xml
+python -m nose2 -s tests test_qgis_environment #| mv nose2-junit.xml test-reports/today/test_qgis_environment.xml
+python -m nose2 -s tests test_qgis_interaction #| mv nose2-junit.xml test-reports/today/test_qgis_interaction.xml
+python -m nose2 -s tests test_resources #| mv nose2-junit.xml test-reports/today/test_resources.xml
+python -m nose2 -s tests test_settings #| mv nose2-junit.xml test-reports/today/test_settings.xml
+python -m nose2 -s tests test_stackedbandinput #| mv nose2-junit.xml test-reports/today/test_stackedbandinput.xml
+python -m nose2 -s tests test_temporalprofiles #| mv nose2-junit.xml test-reports/today/test_temporalprofiles.xml
+python -m nose2 -s tests test_timeseries #| mv nose2-junit.xml test-reports/today/test_timeseries.xml
+python -m nose2 -s tests test_utils #| mv nose2-junit.xml test-reports/today/test_utils.xml
