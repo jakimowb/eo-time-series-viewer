f30c0245e5a637189b613277d97c132b56c90daa
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2020-11-07 00:59:14 +0100

    fixed tests

--- tests/test_mapvisualization.py
+++ tests/test_mapvisualization.py
@@ -310,18 +310,6 @@ class TestMapVisualization(EOTSVTestCase):
 
         self.assertEqual(0, bandClosestToWavelength(lyr, 'B'))
 
-    def test_mapview_context_menu(self):
-
-        from eotimeseriesviewer.main import EOTimeSeriesViewer
-        eotsv = EOTimeSeriesViewer()
-        eotsv.loadExampleTimeSeries(loadAsync=False)
-
-        mv = eotsv.mapViews()[0]
-        self.assertIsInstance(mv, MapView)
-        for l in mv.layers():
-            eotsv.setCurrentLayer(l)
-            mv.mMapLayerTreeViewMenuProvider.populateContextMenu()
-
     def test_renderer(self):
         styleFiles = file_search(os.path.dirname(__file__), 'style*.txt')
 
