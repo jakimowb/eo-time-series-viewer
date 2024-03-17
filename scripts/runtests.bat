0db1da547d71eacb437cb40858e38fb8812ef0ca
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2024-03-17 22:34:18 +0100

    testing

--- scripts/runtests.bat
+++ scripts/runtests.bat
@@ -1,4 +1,6 @@
-#!/bin/bash
 set QT_QPA_PLATFORM=offscreen
 set CI=True
-pytest --no-cov-on-fail --cov-config=%CD%\.coveragec
+set QGIS_CONTINUOUS_INTEGRATION_RUN=true
+rmdir /s /q test-outputs
+rmdir /s /q test-reports
+pytest --no-cov-on-fail --cov-config=.coveragec %*
--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 export QT_QPA_PLATFORM=offscreen
 export CI=True
-pytest --no-cov-on-fail --cov-config=..\.coveragec
+export QGIS_CONTINUOUS_INTEGRATION_RUN=true
+export PYTHONPATH="${PYTHONPATH}"\
+":$(pwd)"\
+":/usr/share/qgis/python/plugins"
+
+rm -Rf test-outputs
+rm -Rf test-reports
+pytest --no-cov-on-fail --cov-config=.coveragec "$@"
+coverage-badge -o coverage.svg -f -v
