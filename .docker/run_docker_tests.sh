4c392e587bf8a1ea95f03d591c33cfaa886f050b
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2024-08-02 23:38:50 +0200

    changes lfs checkout in gh action

--- .docker/run_docker_tests.sh
+++ .docker/run_docker_tests.sh
@@ -21,7 +21,6 @@ ls -l
 export QT_QPA_PLATFORM=offscreen
 export CI=True
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-git lfs checkout
 ls -lisa example/images
 python3 scripts/setup_repository.py -r
 source scripts/runtests.sh
