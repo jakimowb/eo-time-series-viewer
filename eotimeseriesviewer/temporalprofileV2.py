95c3ed48426707f24808e84034f58a00deee6965
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2024-09-28 10:22:11 +0200

    WIP: TemporalProfileV2, dictionary/JSON-based multi-sensor profiles

---
+++ eotimeseriesviewer/temporalprofileV2.py
@@ -0,0 +1,251 @@
+import json
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import List, Optional, Tuple, Union
+
+from eotimeseriesviewer.dateparser import ImageDateUtils
+from eotimeseriesviewer.tasks import EOTSVTask
+from eotimeseriesviewer.timeseries import sensor_id, sensorIDtoProperties
+from qgis.core import (Qgis, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsField, QgsPointXY,
+                       QgsRasterDataProvider, QgsRasterLayer)
+
+
+# TimeSeriesProfileData JSON Format
+# { sensors_ids = [sid 1 <str>, ..., sid n],
+#   sources = [n source strings],
+#   dates = [n date-time-stamps],
+#   sensors = [n sensor id indices],
+#   band_values = [n band value dictionaries]
+# }
+#
+#
+#
+#
+
+class TemporalProfileUtils(object):
+    Source = 'source'  # optional
+    Date = 'date'
+    SensorIDs = 'sensor_ids'
+    Sensor = 'sensor'
+    Values = 'values'
+
+    @staticmethod
+    def isProfileDict(d: dict) -> bool:
+        for k in [TemporalProfileUtils.Date,
+                  TemporalProfileUtils.SensorIDs,
+                  TemporalProfileUtils.Sensor,
+                  TemporalProfileUtils.Values]:
+            if k not in d:
+                return False
+
+        return True
+
+    @staticmethod
+    def verifyProfile(profileDict: dict) -> Tuple[bool, str]:
+
+        n = len(profileDict[TemporalProfileUtils.Date])
+        sensorIds = profileDict[TemporalProfileUtils.SensorIDs]
+        nSensors = len(sensorIds)
+
+        optionalSource = TemporalProfileUtils.Source in profileDict
+
+        for sid in sensorIds:
+            try:
+                sdict = sensorIDtoProperties(sid)
+            except Exception as ex:
+                return False, f'Invalid sensor ID string: {sid}\n{ex}'
+
+        for i, (date, sensor, values) in enumerate(zip(profileDict[TemporalProfileUtils.Date],
+                                                       profileDict[TemporalProfileUtils.Sensor],
+                                                       profileDict[TemporalProfileUtils.Values])):
+            try:
+                dtg = datetime.fromisoformat(date)
+                assert dtg is not None
+                assert 0 <= sensor < nSensors
+                if optionalSource:
+                    src = profileDict[TemporalProfileUtils.Source][i]
+                    assert isinstance(src, str), f'profile source {src} is not string'
+
+            except Exception as ex:
+                return False, f'Item {i + 1}: {ex}\n{date},{sensor},{values}'
+
+        return True, None
+
+    @staticmethod
+    def profileJsonFromDict(d: dict) -> str:
+        txt = json.dumps(d)
+        return txt
+
+    @staticmethod
+    def profileDictFromJson(json_string: str) -> dict:
+        data = json.loads(json_string)
+        return data
+
+    @staticmethod
+    def createTemporalProfileField(name: str) -> QgsField:
+        field = QgsField(name, typeName='JSON')
+        field.setMetadata(Qgis.FieldMetadataProperty.CustomProperty + 1, 'EOTSV')
+        return field
+
+
+class LoadTimeSeriesProfileData(EOTSVTask):
+
+    def __init__(self,
+                 sources: List[Union[str, Path]],
+                 points: List[QgsPointXY],
+                 crs: QgsCoordinateReferenceSystem,
+                 info: dict = None,
+                 cache: dict = None,
+                 *args, **kwds):
+        super().__init__(*args, **kwds)
+
+        self.mInfo = info.copy() if info else None
+        self.mSources: List[Path] = [Path(s) for s in sources]
+        self.mPoints = points[:]
+        self.mCrs = crs
+        self.mProfiles = []
+        self.mProgressInterval = timedelta(seconds=1)
+        self.mLoadingTime: Optional[timedelta] = None
+        self.mCache: dict = cache.copy() if isinstance(cache, dict) else dict()
+        self.mTimeIt: dict = dict()
+
+    def loadingTime(self) -> Optional[timedelta]:
+        return self.mLoadingTime
+
+    def profiles(self) -> List[dict]:
+        return self.mProfiles
+
+    def profilePoints(self) -> List[QgsPointXY]:
+        return self.mPoints
+
+    def timeIt(self, key: str, datetime: datetime):
+        l = self.mTimeIt.get(key, [])
+        l.append(datetime.now() - datetime)
+        self.mTimeIt[key] = l
+
+    def run(self) -> bool:
+        errors = []
+        t0 = datetime.now()
+        tNextProgress = datetime.now() + self.mProgressInterval
+
+        nTotal = len(self.mSources)
+
+        profiles = [{TemporalProfileUtils.Source: [],
+                     TemporalProfileUtils.Date: [],
+                     TemporalProfileUtils.Sensor: [],
+                     TemporalProfileUtils.SensorIDs: [],
+                     TemporalProfileUtils.Values: [],
+                     }
+                    for _ in self.mPoints]
+
+        sensor_ids = []
+
+        for i, src in enumerate(self.mSources):
+            if self.isCanceled():
+                return False
+
+            t1 = datetime.now()
+            options = QgsRasterLayer.LayerOptions(loadDefaultStyle=False)
+            lyr = QgsRasterLayer(src.as_posix(), options=options)
+            self.timeIt('initLayer', t1)
+
+            if not lyr.isValid():
+                errors.append(f'Unable to load {src}')
+                continue
+
+            t1 = datetime.now()
+            if lyr.source() in self.mCache:
+                sid, dtg = self.mCache[lyr.source()]
+            else:
+                sid = sensor_id(lyr)
+                if not sid:
+                    errors.append(f'Unable to load sensor id from {lyr}')
+                    continue
+
+                dtg = ImageDateUtils.datetimeFromLayer(lyr)
+                if not dtg:
+                    errors.append(f'Unable to load date-time from {lyr}')
+                    continue
+
+                self.mCache[lyr.source()] = (sid, dtg)
+            self.timeIt('sid_dtg', t1)
+
+            if sid not in sensor_ids:
+                sensor_ids.append(sid)
+
+            dp: QgsRasterDataProvider = lyr.dataProvider()
+
+            if lyr.crs() == self.mCrs:
+                pts = self.mPoints
+            else:
+                trans = QgsCoordinateTransform()
+                trans.setSourceCrs(self.mCrs)
+                trans.setDestinationCrs(lyr.crs())
+                pts = [trans.transform(pt) for pt in self.mPoints]
+                s = ""
+
+            for i_pt, pt in enumerate(pts):
+                if not lyr.extent().contains(pt):
+                    continue
+
+                profile: dict = profiles[i_pt]
+                values = None
+
+                if False:
+                    t1 = datetime.now()
+                    data = dp.identify(pt, Qgis.RasterIdentifyFormat.Value)
+                    self.timeIt('identify', t1)
+
+                    if data.isValid():
+                        bandValues = data.results()
+                        if len(bandValues) > 0:
+                            valuesI = list(bandValues.values())
+
+                if True:
+                    # from benchmark, loading full-band time series with 100 images:
+                    # Avg initLayer: 0:00:00.014435
+                    # Avg sid_dtg: 0:00:00.006734
+                    # Avg identify: 0:00:00.173399 <- takes too long, total duration 19.sec
+                    # Avg sample: 0:00:00.000056 <- better use dataprovider.sample, total duration 17 sec
+                    t1 = datetime.now()
+                    data = [dp.sample(pt, b + 1) for b in range(dp.bandCount())]
+                    self.timeIt('sample', t1)
+                    values = [None if not d[1] else d[0] for d in data]
+
+                # assert valuesI == values
+
+                if not any(values):
+                    continue
+
+                # we have at least one -none-masked band value
+                profile[TemporalProfileUtils.Source].append(src.as_posix())
+                profile[TemporalProfileUtils.Values].append(values)
+                profile[TemporalProfileUtils.Sensor].append(sensor_ids.index(sid))
+                profile[TemporalProfileUtils.Date].append(dtg.isoformat())
+
+            tNow = datetime.now()
+            if tNow > tNextProgress:
+                progress = (i + 1) / nTotal * 100
+                self.setProgress(progress)
+                tNextProgress = tNow + self.mProgressInterval
+
+        # keep only profile dictionaries for which we have at least one values
+        for p in profiles:
+            p[TemporalProfileUtils.SensorIDs] = sensor_ids
+        profiles = [p for p in profiles if len(p[TemporalProfileUtils.Source]) > 0]
+        self.mProfiles = profiles
+        self.setProgress(100)
+        self.mLoadingTime = datetime.now() - t0
+
+        for k in self.mTimeIt.keys():
+            deltas = self.mTimeIt[k]
+            if len(deltas) > 1:
+                d = deltas[0]
+                for d2 in deltas[1:]:
+                    d += d2
+            else:
+                d = deltas[0]
+
+            self.mTimeIt[k] = d / len(deltas)
+
+        return True
---
+++ tests/test_temporalprofilesV2.py
@@ -0,0 +1,107 @@
+# coding=utf-8
+
+__author__ = 'benjamin.jakimow@geo.hu-berlin.de'
+__date__ = '2024-09-08'
+__copyright__ = 'Copyright 2024, Benjamin Jakimow'
+
+import os
+import unittest
+from pathlib import Path
+
+from eotimeseriesviewer.force import FORCEUtils
+from eotimeseriesviewer.qgispluginsupport.qps.utils import SpatialPoint
+from eotimeseriesviewer.temporalprofileV2 import LoadTimeSeriesProfileData, TemporalProfileUtils
+from eotimeseriesviewer.tests import EOTSVTestCase, start_app
+from qgis.core import QgsCoordinateReferenceSystem, QgsRasterLayer
+
+start_app()
+
+
+class TestTemporalProfilesV2(EOTSVTestCase):
+    """Test temporal profiles"""
+
+    def test_load_timeseries_profiledata(self):
+        files = self.exampleRasterFiles()[1:]
+
+        lyr1 = QgsRasterLayer(files[0])
+        self.assertTrue(lyr1.isValid())
+        pt = SpatialPoint.fromMapLayerCenter(lyr1)
+
+        crs = QgsCoordinateReferenceSystem('EPSG:4326')
+        pt = pt.toCrs(crs)
+        points = [pt]
+
+        nFiles = len(files)
+        info = dict(id=1)
+        task = LoadTimeSeriesProfileData(files, points, crs=crs, info=info)
+        task.run()
+
+        profiles = task.profiles()
+
+        self.assertEqual(len(profiles), len(points))
+        for profile in profiles:
+            self.assertIsInstance(profile, dict)
+
+            sources = profile[TemporalProfileUtils.Source]
+            values = profile[TemporalProfileUtils.Values]
+            for src, val in zip(sources, values):
+                lyr = QgsRasterLayer(src)
+                self.assertTrue(lyr.isValid())
+                self.assertEqual((len(val)), lyr.bandCount())
+                self.assertTrue(None not in val)
+            profileJson = TemporalProfileUtils.profileJsonFromDict(profile)
+            profile2 = TemporalProfileUtils.profileDictFromJson(profileJson)
+
+            self.assertEqual(profile, profile2)
+
+    @unittest.skipIf('FORCE_CUBE' not in os.environ, 'FORCE_CUBE is undefined')
+    def test_load_profile_FORCE(self):
+
+        def onProgress(progress):
+
+            if progress < 100:
+                print(f'\r{progress: 0.2f}', end='')
+            else:
+                print(f'\r{progress: 0.2f}')
+
+        path_cube = Path(os.environ['FORCE_CUBE'])
+        assert path_cube.is_dir()
+        for tileDir in FORCEUtils.tileDirs(path_cube):
+            files = FORCEUtils.productFiles(tileDir, 'BOA')
+            lyr = QgsRasterLayer(files[0].as_posix())
+            self.assertTrue(lyr.isValid())
+            pt = SpatialPoint.fromMapLayerCenter(lyr)
+            files = files[0:5]
+            task = LoadTimeSeriesProfileData(files, [pt], pt.crs())
+            task.progressChanged.connect(onProgress)
+            task.run()
+            cache = task.mCache
+            profiles1 = task.profiles()
+            for p in profiles1:
+                self.assertTrue(TemporalProfileUtils.isProfileDict(p))
+                success, err = TemporalProfileUtils.verifyProfile(p)
+                self.assertTrue(success, msg=err)
+            print(f'Run 1: tile {tileDir}: \nduration {task.loadingTime()}')
+            nTotal = len(files)
+            for k, v in task.mTimeIt.items():
+                print(f'Avg {k}: {v}')
+
+            task = LoadTimeSeriesProfileData(files, [pt], pt.crs(), cache=cache)
+            task.progressChanged.connect(onProgress)
+            task.run()
+            profiles2 = task.profiles()
+            print(f'Run 2: tile {tileDir} (cached layer infos): \nduration: {task.loadingTime()}')
+            nTotal = len(files)
+            for k, v in task.mTimeIt.items():
+                print(f'Avg {k}: {v}')
+
+            s = ""
+        s = ""
+
+    def test_load_timeseries_profiledata_async(self):
+
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main(buffer=False)
