4768ec43859f02f4870ef01ec6c05d35bbb10e22
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-01-08 07:22:58 +0100

    added callback ref to EOTSVTask

--- eotimeseriesviewer/tasks.py
+++ eotimeseriesviewer/tasks.py
@@ -2,5 +2,11 @@ from qgis.core import QgsTask
 
 
 class EOTSVTask(QgsTask):
-    def __init__(self, *args, **kwds):
+    def __init__(self, *args, callback=None, **kwds):
         super().__init__(*args, **kwds)
+
+        self.mCallback = callback
+
+    def finished(self, result):
+        if self.mCallback is not None:
+            self.mCallback(result, self)
