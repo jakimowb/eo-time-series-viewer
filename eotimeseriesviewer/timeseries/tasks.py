20f7890a3d0dd7c95118f5bb856c93f7da8b2d7c
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-03-28 12:34:46 +0100

    cleanup

---
+++ eotimeseriesviewer/timeseries/tasks.py
@@ -0,0 +1,280 @@
+import math
+import warnings
+from typing import Dict, List, Tuple
+
+from qgis.core import Qgis, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsCoordinateTransformContext, \
+    QgsProject, QgsRasterBandStats, QgsRasterLayer, QgsRectangle, QgsTask
+from qgis.PyQt.QtCore import pyqtSignal, QDateTime
+
+from eotimeseriesviewer.qgispluginsupport.qps.utils import SpatialExtent
+from eotimeseriesviewer.tasks import EOTSVTask
+from eotimeseriesviewer.timeseries.source import TimeSeriesSource
+
+
+class TimeSeriesFindOverlapSubTask(QgsTask):
+    """
+    A task to check which time series sources have valid data within a give spatial extent
+    """
+    foundSourceOverlaps = pyqtSignal(dict)
+    executed = pyqtSignal(bool, dict)
+    emptyStats: QgsRasterBandStats = QgsRasterBandStats()
+
+    def __init__(self,
+                 extent: QgsRectangle,
+                 crs: QgsCoordinateReferenceSystem,
+                 sources: List[str],
+                 sample_size: int = 16):
+        super().__init__(flags=QgsTask.CancelWithoutPrompt)
+        assert isinstance(extent, QgsRectangle)
+        assert isinstance(crs, QgsCoordinateReferenceSystem)
+        assert isinstance(sources, list)
+        assert isinstance(sample_size, int) and sample_size > 0
+
+        self.extent = extent
+        self.sources = [str(s) for s in sources]
+        self.crs = crs
+        self.sample_size = sample_size
+        self.errors: List[str] = []
+        self.transformContext: QgsCoordinateTransformContext = QgsCoordinateTransformContext(
+            QgsProject.instance().transformContext())
+        self.intersections: Dict[str, bool] = dict()
+
+    def canCancel(self):
+        return True
+
+    def hasValidPixel(self, source: str) -> bool:
+
+        options = QgsRasterLayer.LayerOptions(loadDefaultStyle=False, transformContext=self.transformContext)
+        lyr = QgsRasterLayer(source, options=options)
+        if not lyr.isValid():
+            self.errors.append(f'Unable to open {source} {lyr.error()}')
+            return False
+
+        ext = QgsRectangle(self.extent)
+        if self.crs != lyr.crs():
+            trans = QgsCoordinateTransform(self.crs, lyr.crs(), self.transformContext)
+            if not trans.isValid():
+                self.errors.append(f'Unable to get coordinate transformation from {self.crs.description()} '
+                                   f'to {lyr.crs().description()}: {lyr.source()}')
+                return False
+            ext = trans.transformBoundingBox(ext)
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=DeprecationWarning)
+            # get band statistics for 1st band in given spatial extent, based on n random samples
+            stats: QgsRasterBandStats = lyr.dataProvider().bandStatistics(1,
+                                                                          stats=Qgis.RasterBandStatistic.Range,
+                                                                          extent=ext,
+                                                                          sampleSize=self.sample_size)
+
+        if not isinstance(stats, QgsRasterBandStats):
+            return False
+
+        return (stats.minimumValue, stats.maximumValue) != (self.emptyStats.minimumValue, self.emptyStats.maximumValue)
+
+    def run(self) -> bool:
+
+        n_total = len(self.sources)
+
+        intersections = dict()
+        for i, source in enumerate(self.sources):
+
+            if self.isCanceled():
+                return False
+
+            intersections[source] = self.hasValidPixel(source)
+            if i % 10 == 0 or i >= n_total - 1:
+                self.setProgress(100 * (i + 1) / n_total)
+                self.foundSourceOverlaps.emit(intersections.copy())
+                self.intersections.update(intersections)
+                intersections.clear()
+
+        assert len(intersections) == 0
+        assert len(self.intersections) == len(self.sources)
+        self.executed.emit(True, self.intersections)
+        return True
+
+
+class TimeSeriesFindOverlapTask(EOTSVTask):
+    sigTimeSeriesSourceOverlap = pyqtSignal(dict)
+
+    executed = pyqtSignal(bool, dict)
+
+    def __init__(self,
+                 extent: SpatialExtent,
+                 time_series_sources: List[TimeSeriesSource],
+                 date_of_interest: QDateTime = None,
+                 sample_size: int = 16,
+                 n_threads: int = 4,
+                 description: str = None):
+        """
+
+        :param extent:
+        :param time_series_sources:
+        :param date_of_interest: date of interest from which to start searching. "pivot" date
+        :param max_forward: max. number of intersecting dates to search into the past of date_of_interest
+            defaults to -1 = search for all dates < date_of_interest
+        :param max_backward: max. number of intersecting dates to search into the future of date_of_interest
+            defaults to -1 = search for all dates > date_of_interest
+        :param callback:
+        :param description:
+        :param sample_size: number of samples in x and y direction
+        :param progress_interval:
+        """
+        if description is None:
+            if date_of_interest:
+                description = f'Find image overlap ({str(date_of_interest)})'
+            else:
+                description = 'Find image overlap'
+
+        super().__init__(description=description,
+                         flags=QgsTask.CancelWithoutPrompt | QgsTask.Silent)
+        assert sample_size >= 1
+        assert isinstance(extent, SpatialExtent)
+
+        sources = list(time_series_sources)
+
+        if isinstance(date_of_interest, QDateTime):
+            sources = sorted(sources, key=lambda tss: abs(date_of_interest.secsTo(tss.dtg())))
+
+        self.mExtent = QgsRectangle(extent)
+        self.mCrs = extent.crs()
+        self.mSampleSize = sample_size
+        self.mIntersections: Dict[str, bool] = dict()
+        self.mErrors: List[str] = []
+        self.mErrors: List[str] = []
+
+        n_sources = len(sources)
+        n_badge = math.ceil(n_sources / n_threads)
+
+        current_badge = []
+        for s in sources:
+            current_badge.append(s.source())
+            if len(current_badge) >= n_badge:
+                subTask = TimeSeriesFindOverlapSubTask(
+                    QgsRectangle(self.mExtent),
+                    QgsCoordinateReferenceSystem(self.mCrs),
+                    current_badge[:],
+                    sample_size=self.mSampleSize
+                )
+                subTask.foundSourceOverlaps.connect(self.sigTimeSeriesSourceOverlap)
+                subTask.executed.connect(self.subTaskExecuted)
+                self.addSubTask(subTask, subTaskDependency=QgsTask.SubTaskDependency.ParentDependsOnSubTask)
+                current_badge.clear()
+
+    def errors(self) -> List[str]:
+        return self.mErrors[:]
+
+    def subTaskExecuted(self, success: bool, results: dict):
+        if success:
+            self.mIntersections.update(results)
+
+    def run(self):
+        """
+        Start the Task and returns the results.
+        :return:
+        """
+
+        for subTask in self.subTasks():
+            subTask: TimeSeriesFindOverlapSubTask
+            self.mIntersections.update(subTask.intersections)
+            self.mErrors.extend(subTask.errors)
+        self.executed.emit(True, self.mIntersections.copy())
+
+        return True
+
+    def canCancel(self) -> bool:
+        return True
+
+
+class TimeSeriesLoadingSubTask(QgsTask):
+    imagesLoaded = pyqtSignal(list)
+
+    def __init__(self,
+                 sources: List[str], *args,
+                 report_block_size: int = 25, **kwds):
+        super().__init__(*args, **kwds)
+        assert 0 < report_block_size
+        self.report_block_size = report_block_size
+        self.sources = [str(s) for s in sources]
+        self.invalid_sources: List[Tuple[str, Exception]] = []
+        self.valid_sources: List[TimeSeriesSource] = []
+
+    def run(self):
+
+        n_total = len(self.sources)
+        block: List[TimeSeriesSource] = []
+        for i, source in enumerate(self.sources):
+            try:
+                tss = TimeSeriesSource.create(source)
+                assert isinstance(tss, TimeSeriesSource), f'Unable to open {source} as TimeSeriesSource'
+                # self.mSources.append(tss)
+                self.valid_sources.append(tss)
+                block.append(tss)
+                del tss
+                if len(block) >= self.report_block_size:
+                    self.imagesLoaded.emit([tss.clone() for tss in block])
+                    block.clear()
+            except Exception as ex:
+                self.invalid_sources.append((source, ex))
+
+            if i % 10 == 0:
+                self.setProgress(100 * (i + 1) / n_total)
+
+        if len(block) > 0:
+            self.imagesLoaded.emit([tss.clone() for tss in block])
+
+        self.setProgress(100.0)
+        return True
+
+
+class TimeSeriesLoadingTask(EOTSVTask):
+    sigFoundSources = pyqtSignal(list)
+
+    def __init__(self,
+                 files: List[str],
+                 description: str = "Load Images",
+                 callback=None,
+                 report_block_size=20,
+                 n_threads: int = 4,
+                 progress_interval: int = 5):
+
+        super().__init__(description=description,
+                         flags=QgsTask.Silent | QgsTask.CanCancel | QgsTask.CancelWithoutPrompt)
+
+        assert progress_interval >= 1
+
+        n_badge = math.ceil(len(files) / n_threads)
+
+        badge = []
+        n_files = len(files)
+        for i, file in enumerate(files):
+            badge.append(file)
+            if len(badge) >= n_badge or i == (n_files - 1):
+                subTask = TimeSeriesLoadingSubTask(badge[:],
+                                                   report_block_size=report_block_size,
+                                                   description=self.description())
+                subTask.imagesLoaded.connect(self.sigFoundSources)
+                badge.clear()
+                self.addSubTask(subTask, subTaskDependency=QgsTask.SubTaskDependency.ParentDependsOnSubTask)
+
+        self.mCallback = callback
+        self.mInvalidSources: List[Tuple[str, Exception]] = []
+        self.mValidSources: List[TimeSeriesSource] = []
+
+    def canCancel(self) -> bool:
+        return True
+
+    def run(self) -> bool:
+        result_block: List[TimeSeriesSource] = []
+
+        for subTask in self._sub_tasks:
+            assert isinstance(subTask, TimeSeriesLoadingSubTask)
+            self.mInvalidSources.extend(subTask.invalid_sources)
+            self.mValidSources.extend(subTask.valid_sources)
+
+        return True
+
+    def finished(self, result):
+        if self.mCallback is not None:
+            self.mCallback(result, self)
