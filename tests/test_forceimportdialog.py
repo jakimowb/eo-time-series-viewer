e290b9276e8399ac8f605054eeb7b0f2ee0b1fa3
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-04-02 07:48:37 +0200

    created test_benchmarks.py

--- tests/test_forceimportdialog.py
+++ tests/test_forceimportdialog.py
@@ -1,19 +1,14 @@
-import datetime
-import json
+import os
 import os
 import unittest
 from pathlib import Path
 
-from qgis._core import QgsApplication, QgsProcessingAlgRunnerTask, QgsProject, QgsRasterLayer, QgsTaskManager
+from qgis.PyQt.QtCore import QDate
 
-from eotimeseriesviewer.force import FORCEUtils
 from eotimeseriesviewer.forceinputs import find_tile_folders, FindFORCEProductsTask, FORCEProductImportDialog, \
     read_tileids, rx_FORCE_TILEFOLDER
 from eotimeseriesviewer.main import EOTimeSeriesViewer
-from eotimeseriesviewer.processingalgorithms import ReadTemporalProfiles
 from eotimeseriesviewer.tests import EOTSVTestCase, start_app
-from qgis import processing
-from qgis.PyQt.QtCore import QDate
 
 start_app()
 
@@ -127,136 +122,6 @@ class FORCEImportTestCases(EOTSVTestCase):
         eotsv.ui.show()
         eotsv.loadFORCEProducts(force_cube=FORCE_CUBE, tile_ids='X0066_Y0058')
         self.showGui(eotsv.ui)
-
-    @unittest.skipIf(not FORCE_CUBE.is_dir(), 'Missing FORCE_CUBE')
-    @unittest.skipIf(EOTSVTestCase.runsInCI(), 'Benchmark only')
-    def test_benchmark_load_eotsv(self):
-
-        path_results = self.createTestOutputDirectory() / 'benchmark_load_eotsv.json'
-
-        if not path_results.is_file():
-            task = FindFORCEProductsTask('BOA', FORCE_CUBE, tile_ids=['X0066_Y0058'])
-            task.run_task_manager()
-
-            files = task.files()
-
-            n_threads = [2, 4, 6]
-            n_files = [50, 100, 200, 400, 800]
-            n_files = [n for n in n_files if n < len(files)]
-            results = dict()
-            for nt in n_threads:
-                for nf in n_files:
-                    files_to_load = files[0:nf]
-                    eotsv = EOTimeSeriesViewer()
-                    eotsv.ui.show()
-                    self.taskManagerProcessEvents()
-                    t0 = datetime.datetime.now()
-                    eotsv.addTimeSeriesImages(files_to_load)
-                    self.taskManagerProcessEvents()
-                    dt = datetime.datetime.now() - t0
-                    results[(nt, nf)] = str(dt)
-
-                    dt_image = dt / len(files_to_load)
-                    print(f'threads={nt}, files={nf}: {dt} -> per file: {dt_image}')
-                    eotsv.close()
-            with open(path_results, 'w') as f:
-                json.dump(results, f, indent=3)
-        else:
-            with open(path_results, 'r') as f:
-                results = json.load(f)
-            print(f'Benchmark results: {path_results}')
-            for (nt, nf), dt in results.items():
-                print(f'threads={nt}, files={nf}: {dt}')
-
-    def test_load_force_processingalgorithm_benchmark(self):
-
-        tiles = [d.name for d in FORCEUtils.tileDirs(FORCE_CUBE)]
-        assert len(tiles) > 0
-        if 'X0066_Y0058' in tiles:
-            tile_id = 'X0066_Y0058'
-        else:
-            tile_id = tiles[0]
-
-        task = FindFORCEProductsTask('BOA', FORCE_CUBE, tile_ids=[tile_id])
-        task.run_task_manager()
-        all_files = task.files()
-
-        assert len(all_files) > 0
-
-        dir_outputs = self.createTestOutputDirectory()
-
-        project = QgsProject()
-
-        path_results = dir_outputs / 'benchmark.json'
-
-        n_threads = [2, 4, 6, 8]
-        n_files = [1, 25, 50, 100, 150, 200, 250, 300]
-        n_files = [n for n in n_files if n < len(all_files)]
-        n_points = [1, 5, 10, 50]
-
-        results = []
-
-        for npts in n_points:
-            path_random_points = dir_outputs / f'random_points_{npts}.geojson'
-            path_random_points.unlink(missing_ok=True)
-
-            for f in all_files:
-                lyr = QgsRasterLayer(f.as_posix())
-                project.addMapLayer(lyr)
-
-                context, feedback = self.createProcessingContextFeedback()
-                context.setProject(project)
-
-                assert lyr.isValid()
-                processing.run("native:randompointsinextent", {
-                    'EXTENT': lyr.extent(),
-                    'POINTS_NUMBER': npts,
-                    'MIN_DISTANCE': 30,
-                    'TARGET_CRS': lyr.crs(),
-                    'MAX_ATTEMPTS': 200,
-                    'OUTPUT': str(path_random_points)},
-                               context=context, feedback=feedback
-                               )
-
-                break
-
-            for nf in n_files:
-                for nt in n_threads:
-                    path_output = dir_outputs / f'files_{nf}_threads_{nt}.geojson'
-                    path_output.unlink(missing_ok=True)
-
-                    alg = ReadTemporalProfiles()
-                    conf = {}
-                    alg.initAlgorithm(conf)
-                    files = [str(f) for f in all_files[0: nf]]
-                    param = {
-                        alg.INPUT: path_random_points.as_posix(),
-                        alg.TIMESERIES: files,
-                        alg.N_THREADS: nt,
-                        alg.FIELD_NAME: 'tp',
-                        alg.OUTPUT: path_output.as_posix(),
-                    }
-
-                    context, feedback = self.createProcessingContextFeedback()
-                    context.setProject(project)
-
-                    task = QgsProcessingAlgRunnerTask(alg, param, context, feedback)
-                    self.assertTrue(task.canCancel())
-                    tm: QgsTaskManager = QgsApplication.taskManager()
-
-                    t0 = datetime.datetime.now()
-                    tm.addTask(task)
-                    self.taskManagerProcessEvents()
-                    dt = datetime.datetime.now() - t0
-
-                    results.append(
-                        {'n_files': nf,
-                         'n_threads': nt,
-                         'n_points': npts,
-                         'seconds': dt.total_seconds()}
-                    )
-                    with open(path_results, 'w') as f:
-                        json.dump(results, f, indent=4)
 
 
 if __name__ == '__main__':
