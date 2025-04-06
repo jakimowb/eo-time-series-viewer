ad37413a26870eec18f5a5656107abbdba3ce5d9
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-04-06 19:49:38 +0000

    testing

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -16,5 +16,11 @@ pwd
 rm -Rf test-outputs
 rm -Rf test-reports
 echo "Run pytest in $(pwd)"
+# not working seeds:
+# python -X faulthandler -m pytest -p no:faulthandler --random-order-seed=397945 --no-cov-on-fail --cov-config=.coveragec "$@"
 pytest --no-cov-on-fail --cov-config=.coveragec "$@"
+# gdb --args python -m pytest  --no-cov-on-fail --cov-config=.coveragec "$@"
+## in gbd -> run
+#     run
+#     bt to show stacktrace after error
 # coverage-badge -o coverage.svg -f -v
