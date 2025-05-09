03af69b1c6772fdefd7e61c91917ee2d7400ca90
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2025-05-09 11:27:34 +0200

    restructured toc, updated images

---
+++ doc/scripts/update_markdown.py
@@ -0,0 +1,45 @@
+import re
+from pathlib import Path
+
+REPO = Path(__file__).parents[2]
+DIR_RST = REPO / 'doc/source'
+
+rx_yaml_block = re.compile(r'^---\s*\n(.*?)\n---\s*\n', re.DOTALL)
+
+
+def update_md(source: Path, target: Path):
+    source = Path(source)
+    target = Path(target)
+    print(f'Update {target}')
+    assert source.is_file()
+    assert source.name.endswith('.md')
+    if target.is_dir():
+        target = target / source.name
+
+    yml_block = ''
+
+    if target.exists():
+        # extract header with rst specific content
+
+        with open(target) as f:
+            match = rx_yaml_block.match(f.read())
+            if match:
+                yml_block = match.group()
+
+    with open(source) as f:
+        data = f.read()
+        data = yml_block + data
+
+    with open(target, 'w', encoding='utf8') as f:
+        f.write(data)
+
+
+if __name__ == '__main__':
+    to_update = [
+        (REPO / 'ABOUT.md', DIR_RST / 'general'),
+        (REPO / 'CHANGELOG.md', DIR_RST / 'general'),
+        (REPO / 'CONTRIBUTORS.md', DIR_RST / 'general'),
+    ]
+
+    for (src, dst) in to_update:
+        update_md(src, dst)
---
+++ doc/source/user_guide/shortcuts.rst
@@ -0,0 +1,29 @@
+==========
+Shortcuts
+==========
+
+This page lists keyboard short-cuts
+
+.. list-table:: Keyboard shortcuts
+   :header-rows: 1
+   :widths: 25 75
+
+   * - Key(s)
+     - Action
+   * - F1 / ALT + F1
+     - set map center from / to QGIS map canvas
+   * - F2 / ALT + F2
+     - set map extent from / to QGIS map canvas
+   * - Arrow Left / Right
+     - move map visualization forward / backward by one date
+   * - Arrow Left / Right + CTRL
+       A / D
+     - move map visualization forward / backward by the number of visible map dates
+   * - End / Pos1
+     - move map visualization to end / start of time series
+   * - Arrow Down / Up
+       S / W
+     - select next / previous vector feature in active attribute table
+   * - Next / Previous Page
+     - show next / previous map view
+       e.g. toggle between a True Color and an Infrared view
